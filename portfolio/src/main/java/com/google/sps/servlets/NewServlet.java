d4d6ca9501de5c5dded6aecfa7e3a990f7bfba91
oba <obafemielegbede@gmail.com>
2021-03-07 05:45:03 +0000

    Fetch JSON list from server

--- portfolio/src/main/java/com/google/sps/servlets/NewServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/NewServlet.java
@@ -15,14 +15,12 @@ public class NewServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
-    
-    
 
     ArrayList<String> quotes = new ArrayList<String>();
-    quotes.add("Do not give up without trying!");
-    quotes.add(" I love gaming!");
-    quotes.add(" Respect is reciprocal!");
-    quotes.add(" Thanks for checking out my portfolio!");
+    quotes.add("Do not give up without trying");
+    quotes.add(" I love gaming");
+    quotes.add(" Respect is reciprocal");
+    quotes.add(" Thanks for checking out my portfolio");
     
 
     String json = convertToJsonUsingGson(quotes);
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -35,40 +35,17 @@ async function getQuotes() {
     welcomeItems.innerHTML = '';
 
 
-    data = []
-    data.append(quote.quotes)
-    data = json.dumps(data)
-
-
     welcomeItems.appendChild(
-      createListElement(quote.quotes));
-    // welcomeItems.appendChild(
-    //   createListElement(quote.quotes[1]));
-    // welcomeItems.appendChild(
-    //   createListElement(quote.quotes[2]));
-    // welcomeItems.appendChild(
-    //   createListElement(quote.quotes[3]));
+      createListElement(quote[0]));
+    welcomeItems.appendChild(
+       createListElement(quote[1]));
+    welcomeItems.appendChild(
+      createListElement(quote[2]));
+    welcomeItems.appendChild(
+      createListElement(quote[3]));
 
 
 }
-
-
-// async function getQuotes() {
-//   const responseFromServer = await fetch('/welcome');
-//   const stats = await responseFromServer.json();
-
-//   const statsListElement = document.getElementById('welcome-container');
-//   statsListElement.innerHTML = '';
-
-//   statsListElement.appendChild(
-//       createListElement(quo));
-//   statsListElement.appendChild(
-//       createListElement('Current time: ' + stats.currentTime));
-//   statsListElement.appendChild(
-//       createListElement('Max memory: ' + stats.maxMemory));
-//   statsListElement.appendChild(
-//       createListElement('Used memory: ' + stats.usedMemory));
-// }
 
 function createListElement(text) {
   const liElement = document.createElement('li');
